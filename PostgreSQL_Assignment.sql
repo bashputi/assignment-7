34e249984e894c5e41be4e3626a4dfe72ec7a60e
rime-islam <rimeislam672@gmail.com>
2024-10-25 16:47:20 +0600

    tables are created

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -1,1 +1,27 @@
 -- Active: 1729850988343@@localhost@4000@university_db
+
+
+-- create "students" table with student information
+CREATE TABLE students (
+    student_id SERIAL PRIMARY KEY,
+    student_name VARCHAR(100) NOT NULL,
+    age INTEGER NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    frontend_mark INTEGER,
+    backend_mark INTEGER,
+    status VARCHAR(20)
+);
+
+-- create 'courses' table with course details
+CREATE TABLE courses (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(50) NOT NULL,
+    credits INTEGER NOT NULL
+);
+
+-- create 'enrollment' table to store enrollments in courses
+CREATE TABLE enrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id INTEGER REFERENCES students(student_id),
+    course_id INTEGER REFERENCES courses(course_id)
+);
